0f02a68d0b2b925cd95927cdcdade5715bf55d2a
AlyssaKirstine <alyssakirstine@gmail.com>
2023-05-29 18:35:46 -0400

    How to Integrate Storybook and Contentful: Chapter 1

---
+++ components/heroModule/HeroModule.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import HeroModule from ".";
+
+const meta: Meta<typeof HeroModule> = {
+  title: "Modules/Hero",
+  component: HeroModule,
+  argTypes: {
+    image: {
+      control: { type: "file" },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof HeroModule>;
+
+export const TextOnly: Story = {
+  args: {
+    header: "Type out header text here",
+    body: "Type out body text here",
+  },
+};
+
+export const WithButton: Story = {
+  args: {
+    header: "Type out header text here",
+    body: "Type out body text here",
+    button: {
+      text: "Button text",
+      href: "#",
+    },
+  },
+};
+
+export const WithImage: Story = {
+  args: {
+    header: "Type out header text here",
+    body: "Type out body text here",
+    button: {
+      text: "Button text",
+      href: "#",
+    },
+    image: "https://dummyimage.com/1276x1282/166434/ffffff&text=Add+image+here",
+  },
+};
